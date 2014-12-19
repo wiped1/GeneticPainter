f28f4d790070a53fa06a76de2b35c985676e00be
Mateusz Dudek <dudek.mateusz92@gmail.com>
2014-12-19 18:19:56 +0100

    Refactored GenotypeCreator.hpp and corresponding tests - uses the same template notation as Genotype

--- GeneticAlgorithm/include/GenotypeCreator.hpp
+++ GeneticAlgorithm/include/GenotypeCreator.hpp
@@ -5,8 +5,9 @@
 /*!
     Abstract class used to create Genotype instances
  */
-template <typename T>
+template <typename T,
+template <typename, typename = std::allocator<T>> class C = std::vector>
 class GenotypeCreator {
 public:
-    virtual Genotype<T> create() const = 0;
+    virtual Genotype<T, C> create() const = 0;
 };
--- GeneticAlgorithm/test/GenotypeCreatorTest.cpp
+++ GeneticAlgorithm/test/GenotypeCreatorTest.cpp
@@ -1,45 +1,64 @@
 #include "Catch/catch.hpp"
 
+#include <list>
+
 #include "GenotypeCreator.hpp"
 
-#ifndef INTEGER_GENOTYPE_CREATOR_H
-#define INTEGER_GENOTYPE_CREATOR_H
-// BEGINNING of test class definition
-class IntegerGenotypeCreator : public GenotypeCreator<int> {
-private:
-    unsigned int _size;
-public:
-    IntegerGenotypeCreator(unsigned int size);
-    virtual Genotype<int> create() const;
-};
+// anonymous namespace to prevent linker errors
+namespace {
+    class IntegerGenotypeCreator : public GenotypeCreator<int> {
+    private:
+        unsigned int _size;
+    public:
+        IntegerGenotypeCreator(unsigned int size) : _size(size) {
+            // do nothing
+        }
+        virtual Genotype<int> create() const {
+            std::vector<int> genes(_size);
+            for ( int gene : genes ) {
+                gene = rand() % 100;
+            }
+            return Genotype<int>(genes);
+        }
+    };
 
-IntegerGenotypeCreator::IntegerGenotypeCreator(unsigned int size) : _size(size) {
-    // do nothing
+    class IntegerListGenotypeCreator : public GenotypeCreator<int, std::list> {
+    private:
+        unsigned int _size;
+    public:
+        IntegerListGenotypeCreator(unsigned int size) : _size(size) {
+            // do nothing
+        }
+        virtual Genotype<int, std::list> create() const {
+            std::list<int> genes(_size);
+            for ( int gene : genes ) {
+                gene = rand() % 100;
+            }
+            return Genotype<int, std::list>(genes);
+        }
+    };
 }
-
-Genotype<int> IntegerGenotypeCreator::create() const {
-    std::vector<int> genes(_size);
-    for ( int gene : genes ) {
-        gene = rand() % 100;
-    }
-    return Genotype<int>(genes);
-}
-// END of test class definition
-#endif
 
 SCENARIO( "GenotypeCreator is used to create Genotype instances" ) {
-    GIVEN( "An GenotypeCreator implementation" ) {
+    GIVEN( "A constant genotype size" ) {
         const unsigned int SIZE = 50;
-        IntegerGenotypeCreator genotypeCreator(SIZE);
 
         WHEN( "A genotype is instantiated using factory" ) {
+            IntegerGenotypeCreator genotypeCreator(SIZE);
             Genotype<int> genotype = genotypeCreator.create();
 
             THEN( "The constructed genotype has correct size" ) {
                 REQUIRE(genotype.getGenes().size() == SIZE);
             }
         }
+        WHEN( "A genotype is instantiated with non-default collection using factory") {
+            IntegerListGenotypeCreator genotypeCreator(SIZE);
+            Genotype<int, std::list> genotype = genotypeCreator.create();
+
+            THEN( "The constructed genotype genes have correct type" ) {
+                REQUIRE(typeid(genotype.getGenes()) == typeid(std::list<int>));
+            }
+        }
     }
 }
-
 
