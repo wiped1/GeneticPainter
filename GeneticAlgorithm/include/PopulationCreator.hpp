99d44d19df283eb89dd33db0e7474e79a143a722
Mateusz Dudek <dudek.mateusz92@gmail.com>
2014-12-21 18:37:50 +0100

    Refactored PopulationCreator and added corresponding test

--- GeneticAlgorithm/include/PopulationCreator.hpp
+++ GeneticAlgorithm/include/PopulationCreator.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <vector>
+
+#include "Population.hpp"
 #include "GenotypeCreator.hpp"
 
 /*!
@@ -14,10 +17,28 @@ private:
     /*!
         Private copy constructor to prevent unwanted initialization
      */
-    PopulationCreator(const PopulationCreator &) { }
+    PopulationCreator(const PopulationCreator&) { }
 
 public:
-    static PopulationCreator & instance();
-    template <typename T>
-    Population<T> createWithSize(unsigned int populationSize, const GenotypeCreator<T> & genotypeCreator);
+    static PopulationCreator& instance();
+    template <typename TGenotype,
+    template <typename, typename = std::allocator<TGenotype>> class TCollection = std::vector>
+    Population<TGenotype, TCollection> createWithSize(unsigned int populationSize,
+            const GenotypeCreator<TGenotype>  &genotypeCreator);
 };
+
+PopulationCreator& PopulationCreator::instance() {
+    static PopulationCreator instance;
+    return instance;
+}
+
+template <typename TGenotype,
+template <typename, typename> class TCollection>
+Population<TGenotype, TCollection> PopulationCreator::createWithSize(unsigned int populationSize,
+        const GenotypeCreator<TGenotype> &genotypeCreator) {
+    TCollection<TGenotype, std::allocator<TGenotype>> genotypes;
+    for ( int i = 0; i < populationSize; i++ ) {
+        genotypes.push_back(genotypeCreator.create());
+    }
+    return Population<TGenotype, TCollection>(genotypes);
+}
