1269e5b17d74ce38c1f006f635ce2ea5956d6cbd
Mckomo <mckomo@gmail.com>
2015-05-23 18:36:33 +0200

    Fixing ImageComparator header

--- include/ImageComparator.hpp
+++ include/ImageComparator.hpp
@@ -25,46 +25,8 @@ private:
 
 public:
 
-    double compareByPixels(const Mat &lhsHsv, const Mat &rhsHsv) const
-    {
-        if (lhsHsv.rows != rhsHsv.rows || lhsHsv.cols != rhsHsv.cols)
-            throw std::invalid_argument("Images must have same size");
-
-        Vec3b lhsPx;
-        Vec3b rhsPx;
-
-        double differenceSum = 0.0;
-        int hueDiff, saturationDiff, valueDiff;
-
-        for (int i = 0; i < lhsHsv.cols; i++)
-        {
-            for (int j = 0; j < lhsHsv.rows; j++)
-            {
-                lhsPx = lhsHsv.at<Vec3b>(j, i);
-                rhsPx = rhsHsv.at<Vec3b>(j, i);
-
-                hueDiff = (90 - std::abs(rhsPx.val[0] - lhsPx.val[0]) % 91) * 4; // 0 to 360
-                saturationDiff = 255 - std::abs(rhsPx.val[1] - lhsPx[1]); // 0 to 255
-                valueDiff = 255 - std::abs(rhsPx.val[2] - lhsPx[2]); // 0 to 255
-
-                differenceSum += hueDiff * saturationDiff * valueDiff;
-
-//                std::cout << "Hue: " << (int) rhsPx.val[0] << ", " << (int) lhsPx.val[0] << std::endl;
-//                std::cout << "Saturation: " << (int) rhsPx.val[1] << ", " << (int) lhsPx.val[1] << std::endl;
-//                std::cout << hueDiff * saturationDiff * valueDiff / 23409000.0 << std::endl;
-//                std::cout << hueDiff << ", " << saturationDiff << ", " << valueDiff << std::endl;
-
-            }
-        }
-
-        return differenceSum / (lhsHsv.cols * lhsHsv.rows * 23409000.0); // 0.0 to 1.0
-    }
-
-    double compareHistograms(MatND &lhs, MatND &rhs)
-    {
-        return cv::compareHist(lhs, rhs, 0);
-    }
-
+    double compareByPixels(const Mat &lhsHsv, const Mat &rhsHsv) const;
+    double compareHistograms(MatND &lhs, MatND &rhs);
     void calculateHistogram(MatND &histogram, const Mat &imageHsv) const;
 
 };
