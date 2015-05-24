ac75a31dca5811ec6b0f820bf618207eb764a36e
Mateusz Dudek <dudek.mateusz92@gmail.com>
2015-05-24 11:02:12 +0200

    Modified mutator behavior

--- src/EllipsesMutationStrategy.cpp
+++ src/EllipsesMutationStrategy.cpp
@@ -205,6 +205,12 @@ struct RemoveFromBackFunctor : MutationFunctor<EllipsesGenotype::Collection, Ell
     }
 };
 
+struct RemoveFromFrontFunctor : MutationFunctor<EllipsesGenotype::Collection, Ellipse> {
+    virtual void operator()(EllipsesGenotype::Collection& genotype, Ellipse& ellipse) const {
+        genotype.erase(std::prev(genotype.end()));
+    }
+};
+
 struct RemoveRandomFunctor : MutationFunctor<EllipsesGenotype::Collection, Ellipse> {
     std::mt19937* prng;
     RemoveRandomFunctor(std::mt19937 &prng) : prng(&prng) {
@@ -223,18 +229,14 @@ EllipsesMutationStrategy::EllipsesMutationStrategy(EllipseGenerator &ellipseGene
         , dist(prng)
 {
     dist.add(new AlterSizeFunctor(ellipseGenerator, prng),10);
-    dist.add(new MakeBiggerFunctor(ellipseGenerator, prng),0);
-    dist.add(new MakeSmallerFunctor(ellipseGenerator, prng),0);
-    dist.add(new AlterColorByRatioFunctor(prng),10);
-    dist.add(new AlterBlueByRatioFunctor(prng),0);
-    dist.add(new AlterGreenByRatioFunctor(prng),0);
-    dist.add(new AlterRedByRatioFunctor(prng),0);
-    dist.add(new AlterColorBySumFunctor(prng),10);
     dist.add(new AlterPositionFunctor(ellipseGenerator, prng),10);
-    dist.add(new SwapWithRandomFunctor(ellipseGenerator),1);
-    dist.add(new AddNewEllipseFunctor(ellipseGenerator),5);
+    dist.add(new AlterColorByRatioFunctor(prng),10);
+    dist.add(new AlterColorBySumFunctor(prng),10);
+    dist.add(new SwapWithRandomFunctor(ellipseGenerator),0.5);
+    dist.add(new AddNewEllipseFunctor(ellipseGenerator),6);
     dist.add(new CopyNewEllipseFunctor(ellipseGenerator, prng), 3);
-    dist.add<RemoveFromBackFunctor>(5);
+    dist.add<RemoveFromBackFunctor>(3);
+    dist.add<RemoveFromFrontFunctor>(3);
     dist.add(new RemoveRandomFunctor(prng),1);
 }
 
