2d5bf5aa1d4f42b42099a7a5ea07c3726611c77e
Mateusz Dudek <dudek.mateusz92@gmail.com>
2015-05-31 15:04:50 +0200

    Parent genotypes are now picked from range [0, size) - to prevent std::next from getting end iterator

--- src/EllipsesBreedingOperator.cpp
+++ src/EllipsesBreedingOperator.cpp
@@ -16,10 +16,10 @@ std::vector<EllipsesGenotype::Type> EllipsesBreedingOperator::breed(const Popula
 {
     std::vector<EllipsesGenotype::Type> parents;
     long populationSize = population.asCollection().size();
-
     for (unsigned int i = 0; i < EvolvingEnvironmentProvider::getInstance().parentsPerChild; i++)
     {
-        long randomIndex = (*prng)() % populationSize;
+        /* populationSize - 1 to prevent randomIndex reaching end iterator */
+        long randomIndex = (*prng)() % (populationSize - 1);
         auto genotype = (*std::next(population.asCollection().cbegin(), randomIndex)).first;
         parents.push_back(genotype);
     }
